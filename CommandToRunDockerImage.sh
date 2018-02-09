2694f4ab10e5f55e25d8b10a0a26d8c04e0a04cb
rickshilling <rick.shilling@gmail.com>
2018-02-08 22:25:29 -0500

    Add files via upload

--- CommandToRunDockerImage.sh
+++ CommandToRunDockerImage.sh
@@ -1,6 +1,8 @@
+xhost +local:root
 docker run -it \
     --env="DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    -v ~/:/home \
     -e USER=root \
-    ricksdockerimage \
+    ort_image \
